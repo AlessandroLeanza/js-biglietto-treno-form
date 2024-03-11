aa816d27f0bceb8fac4c290cf35205b120b21567
AlessandroLeanza <aleleanza91@live.it>
2024-03-11 20:18:31 +0100

    Add: if sconto e sistemato if età passegero

--- js/app.js
+++ js/app.js
@@ -15,27 +15,24 @@ buttonElement.addEventListener("click" , function(){
     
     const km = parseFloat(kmInputElement.value)
     console.log("numero di chilometri da fare: " , km)
-    
-    const eta = parseInt(etaInputElement.value)
-    console.log("La tua età è di: " , eta + " anni")
+    const etaPassegero = parseInt(etaInputElement.value)
+    console.log("La tua età è di: " , etaPassegero + " anni")
     const numeroChilometriPercorsi = km
-    const etaPassegero = eta
     const prezzoInteroDelBiglietto = numeroChilometriPercorsi * 0.21;
-    const scontoMinorenne = prezzoInteroDelBiglietto * 0.2;
-    const prezzoMinorenne = prezzoInteroDelBiglietto - scontoMinorenne;
-    const scontoOverSessantacinque = prezzoInteroDelBiglietto * 0.4;
-    const prezzoOverSessantacinque = prezzoInteroDelBiglietto - scontoOverSessantacinque;
-    const numero = parseFloat(prezzoMinorenne.toFixed(2));
-    const numero2 = parseFloat(prezzoOverSessantacinque.toFixed(2));
-    const numero3 = parseFloat(prezzoInteroDelBiglietto.toFixed(2));
-    if (etaPassegero < 18) {
-        console.log("Sei minorenne quindi paghi: ", numero + "€")
-    } else if (etaPassegero > 65) {
-        console.log("Sei un over 65 quindi paghi: ", numero2 + "€")
-    } else {
-        console.log("Il prezzo del biglietto è di: ", numero3 + "€")
+    let sconto = 0
+    if (etaPassegero<18){
+        sconto = prezzoInteroDelBiglietto * 0.2
+    } else if (etaPassegero>65){
+        sconto = prezzoInteroDelBiglietto * 0.4
     }
-    prezzoInputElement.innerHTML = numero
-
-
+    const prezzo = prezzoInteroDelBiglietto - sconto
+    const result = parseFloat(prezzo.toFixed(2))
+    if (etaPassegero < 18) {
+        prezzoInputElement.innerHTML = "Sei minorenne quindi paghi: " + result
+    } else if (etaPassegero > 65) {
+        prezzoInputElement.innerHTML = "Sei over 65 quindi paghi: " + result
+    } else {
+        prezzoInputElement.innerHTML = "Il prezzo del biglietto è: " + result
+    }
+    
 })
